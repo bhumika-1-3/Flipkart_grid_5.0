65e8e338528194b042eafbae0e1528fa5d2607fe
Bhumika Mange <bhumikamange13@gmail.com>
2023-08-21 01:42:15 +0530

    profile page added integreted

--- src/Page/Customer/Promotion.jsx
+++ src/Page/Customer/Promotion.jsx
@@ -20,34 +20,34 @@ const Promotion = () => {
                 <SplideSlide>
                     <a href="https://www.bajajfinserv.in/investments/fixed-deposit" target="_blank" rel="noreferrer">
                         <Paper style={{ cursor: "pointer" }} sx={{ boxShadow: "none !important", borderRadius: "12px", borderStyle: "solid", borderWidth: "1px", borderColor: "divider", height: "100%", }}>
-                            <img alt="bajaj" src="https://cms-assets.bajajfinserv.in/is/image/bajajfinance/fixed-deposite-v1?scl=1&fmt=png-alpha" width={350} height={180}></img>
+                            <img alt="bajaj" src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/faf94da1d0114e54b66daf7f0122aba5_9366/Graphic_Tee_Pink_IC6046_21_model.jpg" width={350} height={180}></img>
                         </Paper>
                     </a>
                 </SplideSlide>
                 <SplideSlide>
                     <a href="https://www.bajajfinserv.in/rbl-dbs-credit-cards" target="_blank" rel="noreferrer">
                         <Paper sx={{ boxShadow: "none !important", borderRadius: "12px", borderStyle: "solid", borderWidth: "1px", borderColor: "divider", height: "100%", }}>
-                            <img alt="bajaj" src="https://images-eu.ssl-images-amazon.com/images/G/31/img16/vineet/Amazon-Pay-Later/Nov_21/770x350_Bajaj-Finserv.jpg" width={350} height={180}></img>
+                            <img alt="bajaj" src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/cc685791576a4273ad5eaed701484af6_9366/NMD_R1_Shoes_Red_GX9888_01_standard.jpg" width={350} height={180}></img>
                         </Paper>
                     </a>
                 </SplideSlide>
                 <SplideSlide>
                     <a href="https://www.bajajfinserv.in/rbl-dbs-credit-cards" target="_blank" rel="noreferrer">
                         <Paper sx={{ boxShadow: "none !important", borderRadius: "12px", borderStyle: "solid", borderWidth: "1px", borderColor: "divider", height: "100%", }}>
-                            <img alt="bajaj" src="https://cms-assets.bajajfinserv.in/is/image/bajajfinance/credit-card-banner-4?scl=1&fmt=png-alpha" width={350} height={180}></img>
+                            <img alt="bajaj" src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/819a4a352e264f0c9a8aaf120142cc35_9366/AEROREADY_Training_Logo_Graphic_Short_Sleeve_Tee_White_H44740_23_hover_model.jpg" width={350} height={180}></img>
                         </Paper>
                     </a>
                 </SplideSlide>
                 <SplideSlide>
                     <a href="https://www.bajajfinserv.in/emi-network" target="_blank" rel="noreferrer">
                         <Paper sx={{ boxShadow: "none !important", borderRadius: "12px", borderStyle: "solid", borderWidth: "1px", borderColor: "divider", height: "100%", }}>
-                            <img alt="bajaj" src="https://cms-assets.bajajfinserv.in/is/image/bajajfinance/car-insurance-v1-5?scl=1&fmt=png-alpha" width={350} height={180}></img>
+                            <img alt="bajaj" src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/e0b110f58bb54e77bb89ad470114fea0_9366/Face_Cover_-_Not_For_Medical_Use_Black_HC4698_012_hover_standard.jpg" width={350} height={180}></img>
                         </Paper>
                     </a>
                 </SplideSlide>
                 <SplideSlide>
                     <Paper sx={{ boxShadow: "none !important", borderRadius: "12px", borderStyle: "solid", borderWidth: "1px", borderColor: "divider", height: "100%", }}>
-                        <img alt="bajaj" src="https://cms-assets.bajajfinserv.in/is/image/bajajfinance/mutual-fund-v1-1?scl=1&fmt=png-alpha" width={350} height={180}></img>
+                        <img alt="bajaj" src="https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/4ddd69aebdd640d7b41eaedf01011da8_9366/Y-3_Classic_Puffy_Down_Hooded_Coat_Green_HS7455_23_hover_model.jpg" width={350} height={180}></img>
                     </Paper>
                 </SplideSlide>
 
